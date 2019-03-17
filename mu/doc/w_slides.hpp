4e3502c1efd696f1934bbeb5fb31a316f276628a
Petr Rockai <me@mornfall.net>
2019-03-17 20:17:36 +0000

    mu doc: Implement a backend for generating PDF slides via ConTeXt.

---
+++ mu/doc/w_slides.hpp
@@ -0,0 +1,120 @@
+#pragma once
+#include "writer.hpp"
+#include <vector>
+#include <sstream>
+#include <iostream>
+
+/* Writer for ConTeXt-based PDF slides. */
+
+namespace umd::doc
+{
+
+    struct w_slides : writer
+    {
+        stream &out;
+        w_slides( stream &out ) : out( out ) {}
+
+        bool table_rules;
+        int table_rows;
+
+        std::string heading_cmd( int l )
+        {
+            std::stringstream s;
+            s << "\\";
+            while ( l > 1 )
+                --l, s << "sub";
+            s << "section";
+            return s.str();
+        }
+
+        virtual void text( std::u32string_view t ) { out.emit( t ); }
+        virtual void heading( std::u32string_view t, int level )
+        {
+            out.emit( heading_cmd( level ), "[title={", t, "}]" );
+        }
+
+        /* spans ; may be also called within mpost btex/etex */
+        virtual void em_start() { out.emit( "{\\em{}" ); }
+        virtual void em_stop()  { out.emit( "}" ); }
+        virtual void tt_start() { out.emit( "{\\tt{}" ); }
+        virtual void tt_stop()  { out.emit( "}" ); }
+
+        /* lists */
+        virtual void enum_start( int level )
+        {
+            out.emit( "\\startitemize[packed,",
+                      // level == 0 ? 'n' : level == 1 ? 'a' : 'r',
+                      "][distance=-4pt]", "\n" );
+        }
+
+        virtual void enum_item() { out.emit( "\\item " ); }
+        virtual void enum_stop() { out.emit( "\\stopitemize", "\n" ); }
+
+        virtual void bullet_start( int level )
+        {
+            out.emit( "\\startitemize[packed,"
+                      // level == 0 ? 1 : level == 1 ? 5 : 4,
+                      "][distance=-4pt]", "\n" );
+        }
+
+        virtual void bullet_item() { out.emit( "\\item " ); }
+        virtual void bullet_stop() { out.emit( "\\stopitemize", "\n" ); }
+
+        /* metapost figures */
+        virtual void mpost_start()
+        {
+            out.emit( "\\blank[medium]\\startalignment[center]\\strut\\startMPcode", "\n" );
+        }
+
+        virtual void mpost_write( std::string_view sv )
+        {
+            out.emit( sv );
+        }
+
+        virtual void mpost_stop()
+        {
+            out.emit( "\\stopMPcode\\stopalignment\\blank[medium]", "\n" );
+        }
+
+        /* tables */
+        virtual void table_start( columns ci, bool rules )
+        {
+            table_rules = rules;
+            table_rows = 0;
+            out.emit( "\\placetable[force,none]{}{\\blank[-1ex]",
+                      "\\starttabulate[" );
+            for ( auto c : ci )
+                out.emit( "|", c );
+            out.emit( "|]", rules ? "\\HL" : "", "\n" );
+        }
+
+        virtual void table_cell( std::u32string_view c ) { out.emit( "\\NC ", c ); }
+
+        virtual void table_next_row()
+        {
+            out.emit( "\\NR", "\n" );
+            if ( ++table_rows == 1 && table_rules )
+                out.emit( "\\HL", "\n" );
+        }
+
+        virtual void table_stop()
+        {
+            out.emit( table_rules ? "\\HL\n" : "", "\\stoptabulate\\blank[-1ex]}", "\n" );
+        }
+
+        /* blocks */
+        virtual void code_start( std::string type )
+        {
+            out.emit( "\\starttyping[margin=10pt,option=", type, "]", "\n" );
+        }
+
+        virtual void code_line( std::u32string_view l ) { out.emit( l, "\n" ); }
+        virtual void code_stop() { out.emit( "\\stoptyping" ); }
+        virtual void quote_start() { out.emit( "\\startblockquote" ); }
+        virtual void quote_stop() { out.emit( "\\stopblockquote" );  }
+
+        /* paging */
+        virtual void pagebreak() { out.emit( "\\stopmakeup\\startmakeup[slide]", "\n" ); }
+    };
+
+}
---
+++ mu/doc/writer.hpp
@@ -0,0 +1,84 @@
+#pragma once
+#include <vector>
+#include <codecvt>
+
+namespace umd::doc
+{
+
+    struct stream
+    {
+        std::ostream &ostr;
+        stream( std::ostream &o ) : ostr( o ) {}
+
+        void emit() {}
+
+        template< typename T, typename... Ts >
+        void emit( const T &t, const Ts & ... ts );
+
+        template< typename... Ts >
+        void emit( const std::u32string_view &u, const Ts & ... ts )
+        {
+            std::wstring_convert< std::codecvt_utf8< char32_t >, char32_t > conv;
+            ostr << conv.to_bytes( u.begin(), u.end() );
+            emit( ts... );
+        }
+
+        template< typename... Ts >
+        void emit( const std::u32string &u, const Ts & ... ts )
+        {
+            emit( std::u32string_view( u ), ts... );
+        }
+
+    };
+
+    template< typename T, typename... Ts >
+    void stream::emit( const T &t, const Ts & ... ts )
+    {
+        ostr << t;
+        emit( ts... );
+    }
+
+    struct writer
+    {
+        using columns = std::vector< char >;
+
+        virtual void text( std::u32string_view ) = 0;
+        virtual void heading( std::u32string_view txt, int level ) = 0;
+
+        /* spans ; may be also called within mpost btex/etex */
+        virtual void em_start() {}
+        virtual void em_stop() {}
+        virtual void tt_start() {}
+        virtual void tt_stop() {}
+
+        /* lists */
+        virtual void enum_start( int level ) = 0;
+        virtual void enum_item() = 0;
+        virtual void enum_stop() = 0;
+        virtual void bullet_start( int level ) = 0;
+        virtual void bullet_item() = 0;
+        virtual void bullet_stop() = 0;
+
+        /* metapost figures */
+        virtual void mpost_start() = 0;
+        virtual void mpost_write( std::string_view ) = 0;
+        virtual void mpost_stop() = 0;
+
+        /* tables */
+        virtual void table_start( columns, bool ) = 0;
+        virtual void table_cell( std::u32string_view ) = 0;
+        virtual void table_next_row() = 0;
+        virtual void table_stop() = 0;
+
+        /* blocks */
+        virtual void code_start( std::string type ) = 0;
+        virtual void code_line( std::u32string_view ) = 0;
+        virtual void code_stop() = 0;
+        virtual void quote_start() {}
+        virtual void quote_stop() {}
+
+        /* paging */
+        virtual void pagebreak() {}
+    };
+
+}

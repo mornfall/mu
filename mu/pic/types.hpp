6f6dab0d98d429e7df44cd1058d1b44847234181
Petr Rockai <code@fixp.eu>
2019-09-13 08:10:36 +0000

    mu pic: Extend the dir_t enum with 45 degree directions (ne, nw, se, sw).

--- mu/pic/types.hpp
+++ mu/pic/types.hpp
@@ -2,7 +2,8 @@
 
 namespace umd::pic
 {
-    enum dir_t { north = 0, east = 1, south = 2, west = 3 };
+    enum dir_t { north = 0, east = 1, south = 2, west = 3,
+                north_east, north_west, south_west, south_east };
     constexpr const int corner_nw = 0, corner_ne = 1, corner_se = 2, corner_sw = 3;
 
     dir_t opposite( dir_t dir )

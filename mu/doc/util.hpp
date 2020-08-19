5441a4b6d4fcb1879d347bb474088ccbd2afb8a3
Petr Rockai <me@mornfall.net>
2020-08-19 08:49:05 +0000

    mu doc: Rename common.hpp to doc/util.hpp, fix prototypes.

---
+++ mu/doc/util.hpp
@@ -0,0 +1,32 @@
+#pragma once
+#include <string_view>
+#include <locale>
+#include <codecvt>
+#include <fstream>
+
+static inline std::u32string read_file( std::ifstream &in )
+{
+    in.seekg( 0, std::ios::end );
+    size_t length = in.tellg();
+    in.seekg( 0, std::ios::beg );
+
+    std::string buffer;
+    buffer.resize( length );
+    in.read( &buffer[ 0 ], length );
+
+    std::wstring_convert< std::codecvt_utf8< char32_t >, char32_t > conv;
+    return conv.from_bytes( buffer );
+}
+
+static inline std::u32string read_file( std::string path )
+{
+    std::ifstream ifs( path );
+    return read_file( ifs );
+}
+
+static inline std::string to_utf8( std::u32string_view w )
+{
+    std::wstring_convert< std::codecvt_utf8< char32_t >, char32_t > conv;
+    return conv.to_bytes( w.begin(), w.end() );
+}
+

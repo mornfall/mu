389df486d2d0ac46a12dd77fe9f35b32a87be0ef
Petr Rockai <me@mornfall.net>
2020-09-29 00:28:45 +0000

    mu doc: Make read_file() work with stdin.

--- mu/doc/util.hpp
+++ mu/doc/util.hpp
@@ -4,15 +4,20 @@
 #include <codecvt>
 #include <fstream>
 
-static inline std::u32string read_file( std::ifstream &in )
+static inline std::u32string read_file( std::istream &in )
 {
-    in.seekg( 0, std::ios::end );
-    size_t length = in.tellg();
-    in.seekg( 0, std::ios::beg );
-
     std::string buffer;
-    buffer.resize( length );
-    in.read( &buffer[ 0 ], length );
+
+    if ( in.tellg() == -1 )
+        std::getline( in, buffer, '\0' ); /* FIXME binary data */
+    else
+    {
+        in.seekg( 0, std::ios::end );
+        size_t length = in.tellg();
+        in.seekg( 0, std::ios::beg );
+        buffer.resize( length );
+        in.read( &buffer[ 0 ], length );
+    }
 
     std::wstring_convert< std::codecvt_utf8< char32_t >, char32_t > conv;
     return conv.from_bytes( buffer );

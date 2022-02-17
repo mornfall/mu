478fc1e9ff5e42ac48f8b63e0d21fa6d8ed2bb79
Petr Rockai <me@mornfall.net>
2022-02-17 06:03:56 +0000

    gib: Factor out queue management into a separate unit.

--- gib.bundle/main.c
+++ gib.bundle/main.c
@@ -1,20 +1,8 @@
 #include "outdb.h"
 #include "rules.h"
 #include "graph.h"
-#include "job.h"
-#include <sys/wait.h>
+#include "queue.h"
 #include <sys/utsname.h>
-#include <signal.h>
-#include <ctype.h>
-#include <time.h>
-#define MAX_FD 64
-
-static volatile sig_atomic_t _signalled = 0;
-
-void sighandler( int sig )
-{
-    _signalled = sig;
-}
 
 typedef enum
 {
@@ -42,308 +30,20 @@ typedef struct selector
 
 typedef struct
 {
+    queue_t queue;
+
     char *srcdir,
          *outdir;
 
     int outdir_fd;
+    FILE *debug;
 
     cb_tree env;
     cb_tree nodes;
-    cb_tree jobs;
-
-    job_t *job_next, *job_last;
-    job_t *running[ MAX_FD ];
 
     selector_t *select_head, *select_tail;
     cb_tree goals;
-
-    int skipped_count;
-    int failed_count;
-    int ok_count;
-    int todo_count;
-    int queued_count;
-    int running_count;
-    int running_max;
-
-    FILE *debug;
 } state_t;
-
-void job_show_result( state_t *s, node_t *n, job_t *j )
-{
-    const char *status = "??";
-    int color = 0;
-    bool changed = n->stamp_changed == n->stamp_want;
-    bool updated = n->stamp_updated == n->stamp_want;
-
-    if      ( n->failed )      status = "no", color = 31, s->failed_count ++;
-    else if ( !changed )       status = "--", color = 33, s->skipped_count ++;
-    else if ( j && j->warned ) status = "ok", color = 33, s->ok_count ++;
-    else                       status = "ok", color = 32, s->ok_count ++;
-
-    if ( !_signalled && n->failed || changed && j && j->warned )
-    {
-        fprintf( stderr, "\033[J" );
-        char path[ strlen( n->name ) + 13 ];
-        char *p = stpcpy( path, "_log/" );
-        for ( char *i = n->name; *i; ++p, ++i )
-            *p = ( *i == ' ' || *i == '/' ) ? '_' : *i;
-        strcpy( p, ".txt" );
-
-        reader_t log;
-
-        if ( !reader_init( &log, s->outdir_fd, path ) )
-            sys_error( "opening logfile %s", path );
-
-        while ( read_line( &log ) )
-            fprintf( stderr, " │ %.*s\n", span_len( log.span ), log.span.str );
-
-        close( log.fd );
-    }
-
-    s->todo_count --;
-    fprintf( stderr, "\033[J\033[%dm%s\033[0m %s\n", color, status, n->name );
-}
-
-void job_queue( state_t *s, job_t *j )
-{
-    assert( !j->queued );
-    assert( j->node->dirty );
-
-    fprintf( s->debug, "queue: %s\n", j->name );
-
-    j->queued = true;
-    ++ s->queued_count;
-
-    if ( !s->job_next )
-        s->job_next = j;
-
-    if ( s->job_last )
-        s->job_last->next = j;
-
-    s->job_last = j;
-}
-
-bool job_start( state_t *s )
-{
-    if ( !s->job_next )
-        return false;
-
-    job_t *j = s->job_next;
-    s->job_next = j->next;
-
-    cb_clear( &j->node->deps_dyn );
-    job_fork( j, s->outdir_fd );
-
-    s->running_count ++;
-    s->queued_count --;
-
-    assert( j->pipe_fd < MAX_FD );
-    s->running[ j->pipe_fd ] = j;
-
-    return true;
-}
-
-void create_jobs( state_t *s, node_t *goal );
-
-void job_skip( state_t *s, node_t *n )
-{
-    if ( n->failed )
-        return;
-
-    job_show_result( s, n, NULL );
-    n->failed = true;
-
-    for ( cb_iterator i = cb_begin( &n->blocking ); !cb_end( &i ); cb_next( &i ) )
-        job_skip( s, cb_get( &i ) );
-}
-
-void node_cleanup( state_t *s, node_t *n )
-{
-    if ( n->type != out_node && n->type != meta_node || n->dirty || n->waiting )
-        return;
-
-    for ( cb_iterator i = cb_begin( &n->blocking ); !cb_end( &i ); cb_next( &i ) )
-    {
-        node_t *b = cb_get( &i );
-
-        if ( ! -- b->waiting )
-        {
-            if ( b->dirty )
-                job_queue( s, job_add( &s->jobs, b ) );
-            else
-            {
-                node_cleanup( s, b );
-                s->todo_count --;
-            }
-        }
-    }
-
-    cb_clear( &n->blocking );
-}
-
-void job_cleanup( state_t *s, int fd )
-{
-    job_t *j = s->running[ fd ];
-    s->running[ fd ] = 0;
-    node_t *n = j->node;
-
-    int status;
-
-    if ( waitpid( j->pid, &status, 0 ) == -1 )
-        sys_error( "waitpid %d", j->pid );
-
-    if ( WIFEXITED( status ) && WEXITSTATUS( status ) == 0 )
-    {
-        n->stamp_updated = n->stamp_want;
-        n->cmd_hash = var_hash( n->cmd );
-        n->dirty = false;
-
-        if ( j->changed )
-            n->stamp_changed = n->stamp_want;
-    }
-    else
-        n->failed = true;
-
-    s->running_count --;
-    job_show_result( s, n, j );
-
-    for ( cb_iterator i = cb_begin( &j->node->blocking ); !cb_end( &i ); cb_next( &i ) )
-    {
-        node_t *b = cb_get( &i );
-
-        if ( n->stamp_changed > b->stamp_updated )
-            b->dirty = true;
-
-        if ( n->failed && !b->failed )
-            job_skip( s, b );
-
-        if ( -- b->waiting )
-            continue;
-
-        if ( b->dirty && b->stamp_updated < b->stamp_want && !b->failed )
-            job_queue( s, job_add( &s->jobs, b ) );
-        else
-            node_cleanup( s, b );
-    }
-}
-
-void teardown( state_t *s )
-{
-    fprintf( stderr, "\033[J\r[caught signal %d, cleaning up]\n", _signalled );
-    fflush( stderr );
-
-    job_t *j = NULL;
-
-    for ( int fd = 0; fd < MAX_FD; ++ fd )
-        if ( ( j = s->running[ fd ] ) )
-            kill( j->pid, SIGTERM );
-
-    for ( int fd = 0; fd < MAX_FD; ++ fd )
-        if ( s->running[ fd ] )
-            job_cleanup( s, fd );
-}
-
-bool main_loop( state_t *s )
-{
-    fd_set ready, except;
-    FD_ZERO( &ready );
-    FD_ZERO( &except );
-
-    if ( s->running_count )
-    {
-        for ( int i = 0; i < MAX_FD; ++i )
-            if ( s->running[ i ] )
-                FD_SET( i, &ready ), FD_SET( i, &except );
-
-        struct timeval tv = { 1, 0 };
-
-        switch ( select( MAX_FD, &ready, 0, &except, &tv ) )
-        {
-            case 0:
-                return true;
-            case -1:
-                if ( errno != EINTR )
-                    sys_error( "select" );
-            default:
-                ;
-        }
-    }
-
-    if ( _signalled )
-        teardown( s );
-
-    if ( s->running_count )
-        for ( int fd = 0; fd < MAX_FD; ++ fd )
-            if ( FD_ISSET( fd, &ready ) || FD_ISSET( fd, &except ) )
-                if ( job_update( s->running[ fd ], &s->nodes, s->srcdir ) )
-                    job_cleanup( s, fd );
-
-    if ( !_signalled )
-        while ( s->running_count < s->running_max )
-            if ( !job_start( s ) )
-                break;
-
-    return !_signalled && ( s->running_count || s->job_next );
-}
-
-void create_jobs( state_t *s, node_t *goal )
-{
-    if ( goal->visited )
-        return;
-
-    goal->visited = true;
-    node_t *out = goal->type == out_node ? goal : 0;
-
-    for ( cb_iterator i = cb_begin( &goal->deps ); !cb_end( &i ); cb_next( &i ) )
-    {
-        node_t *dep = cb_get( &i );
-        create_jobs( s, dep );
-
-        if ( out && out->stamp_want < dep->stamp_want )
-            out->stamp_want = dep->stamp_want;
-    }
-
-    if ( !out )
-        return;
-
-    if ( var_hash( out->cmd ) != out->cmd_hash )
-        out->dirty = true;
-
-    if ( out->stamp_want > out->stamp_updated || out->dirty )
-    {
-        for ( cb_iterator i = cb_begin( &goal->deps ); !cb_end( &i ); cb_next( &i ) )
-        {
-            node_t *dep = cb_get( &i );
-            node_t *dep_out = dep->type == out_node ? dep : 0;
-
-            if ( dep_out && !dep_out->failed )
-                if ( dep_out->stamp_want > dep_out->stamp_updated || dep_out->dirty )
-                {
-                    cb_insert( &dep->blocking, goal, VSIZE( goal, name ), -1 );
-                    goal->waiting ++;
-                }
-
-            if ( dep->stamp_changed > out->stamp_updated )
-                out->dirty = true;
-        }
-    }
-
-    if ( out->waiting || out->dirty )
-        ++ s->todo_count;
-
-    if ( !out->waiting && out->dirty ) /* can run right away */
-        job_queue( s, job_add( &s->jobs, out ) );
-}
-
-void set_goal( state_t *s, const char *name )
-{
-    node_t *goal = graph_get( &s->nodes, span_lit( name ) );
-
-    if ( goal )
-        create_jobs( s, goal );
-    else
-        error( "goal %s not defined", name );
-}
 
 void state_init( state_t *s )
 {
@@ -351,7 +51,6 @@ void state_init( state_t *s )
 
     cb_init( &s->env );
     cb_init( &s->nodes );
-    cb_init( &s->jobs );
 
     struct utsname uts;
 
@@ -368,19 +67,6 @@ void state_init( state_t *s )
     var_add( uname, span_lit( uts.sysname ) );
 
     s->outdir = "build";
-    s->failed_count = 0;
-    s->skipped_count = 0;
-    s->ok_count = 0;
-    s->running_count = 0;
-    s->queued_count = 0;
-    s->todo_count = 0;
-    s->running_max = 4;
-
-    s->job_next = NULL;
-    s->job_last = NULL;
-
-    for ( int i = 0; i < MAX_FD; ++i )
-        s->running[ i ] = 0;
 }
 
 void state_setup_outputs( state_t *s )
@@ -400,6 +86,8 @@ void state_setup_outputs( state_t *s )
         sys_error( "opening %s/debug for writing", s->outdir );
     else
         s->debug = fdopen( debug_fd, "w" );
+
+    queue_init( &s->queue, s->outdir_fd, s->srcdir );
 }
 
 void state_load( state_t *s )
@@ -414,7 +102,7 @@ void state_load( state_t *s )
     state_setup_outputs( s );
 
     if ( ( jobs = env_get( &s->env, span_lit( "jobs" ) ) ) && jobs->list )
-        s->running_max = atoi( jobs->list->data );
+        s->queue.running_max = atoi( jobs->list->data );
 
     load_dynamic( &s->nodes, s->outdir_fd, "gib.dynamic" );
     load_stamps( &s->nodes, s->outdir_fd, "gib.stamps" );
@@ -429,31 +117,6 @@ void state_save( state_t *s )
 void state_destroy( state_t *s )
 {
     /* … */
-}
-
-void monitor( state_t *s )
-{
-    time_t started = time( NULL );
-    time_t elapsed = 0;
-
-    signal( SIGHUP, sighandler );
-    signal( SIGINT, sighandler );
-    signal( SIGTERM, sighandler );
-
-    signal( SIGPIPE, sighandler ); // ??
-    signal( SIGALRM, sighandler ); // ??
-
-    while ( main_loop( s ) )
-    {
-        elapsed = time( NULL ) - started;
-        fprintf( stderr, "%d/%d running + %d/%d queued | %d ok + %d failed | %lld:%02lld elapsed\r",
-                 s->running_count, s->running_max, s->queued_count, s->todo_count - s->running_count,
-                 s->ok_count, s->failed_count, elapsed / 60, elapsed % 60 );
-    }
-
-    elapsed = time( NULL ) - started;
-    fprintf( stderr, "build finished: %d ok, %d failed, %d skipped, %lld:%02lld elapsed\n",
-             s->ok_count, s->failed_count, s->skipped_count, elapsed / 60, elapsed % 60 );
 }
 
 void selector_init( selector_t *sel, match_op_t op, match_type_t type, const char *arg )
@@ -594,14 +257,14 @@ int main( int argc, char *argv[] )
         }
 
     for ( cb_iterator i = cb_begin( &s.goals ); !cb_end( &i ); cb_next( &i ) )
-        create_jobs( &s, cb_get( &i ) );
+        queue_create_jobs( &s.queue, cb_get( &i ) );
 
     for ( cb_iterator i = cb_begin( &s.nodes ); !cb_end( &i ); cb_next( &i ) )
-        node_cleanup( &s, cb_get( &i ) );
+        queue_cleanup_node( &s.queue, cb_get( &i ) );
 
     graph_dump( s.debug, &s.nodes );
-    monitor( &s );
+    queue_monitor( &s.queue, true );
     state_save( &s );
 
-    return s.failed_count > 0;
+    return s.queue.failed_count > 0;
 }
---
+++ gib.bundle/queue.h
@@ -0,0 +1,361 @@
+#include "outdb.h"
+#include "rules.h"
+#include "graph.h"
+#include "job.h"
+#include <sys/wait.h>
+#include <sys/utsname.h>
+#include <signal.h>
+#include <ctype.h>
+#include <time.h>
+#define MAX_FD 64
+
+static volatile sig_atomic_t _signalled = 0;
+
+void sighandler( int sig )
+{
+    _signalled = sig;
+}
+
+typedef struct
+{
+    const char *srcdir;
+
+    int outdir_fd;
+    time_t started;
+
+    cb_tree *nodes;
+    cb_tree jobs;
+
+    job_t *job_next, *job_last;
+    job_t *running[ MAX_FD ];
+
+    int skipped_count;
+    int failed_count;
+    int ok_count;
+    int todo_count;
+    int queued_count;
+    int running_count;
+    int running_max;
+} queue_t;
+
+void queue_show_result( queue_t *q, node_t *n, job_t *j )
+{
+    const char *status = "??";
+    int color = 0;
+    bool changed = n->stamp_changed == n->stamp_want;
+    bool updated = n->stamp_updated == n->stamp_want;
+
+    if      ( n->failed )      status = "no", color = 31, q->failed_count ++;
+    else if ( !changed )       status = "--", color = 33, q->skipped_count ++;
+    else if ( j && j->warned ) status = "ok", color = 33, q->ok_count ++;
+    else                       status = "ok", color = 32, q->ok_count ++;
+
+    if ( !_signalled && n->failed || changed && j && j->warned )
+    {
+        fprintf( stderr, "\033[J" );
+        char path[ strlen( n->name ) + 13 ];
+        char *p = stpcpy( path, "_log/" );
+        for ( char *i = n->name; *i; ++p, ++i )
+            *p = ( *i == ' ' || *i == '/' ) ? '_' : *i;
+        strcpy( p, ".txt" );
+
+        reader_t log;
+
+        if ( !reader_init( &log, q->outdir_fd, path ) )
+            sys_error( "opening logfile %s", path );
+
+        while ( read_line( &log ) )
+            fprintf( stderr, " │ %.*s\n", span_len( log.span ), log.span.str );
+
+        close( log.fd );
+    }
+
+    q->todo_count --;
+    fprintf( stderr, "\033[J\033[%dm%s\033[0m %s\n", color, status, n->name );
+}
+
+void queue_add( queue_t *q, job_t *j )
+{
+    assert( !j->queued );
+    assert( j->node->dirty );
+
+    j->queued = true;
+    ++ q->queued_count;
+
+    if ( !q->job_next )
+        q->job_next = j;
+
+    if ( q->job_last )
+        q->job_last->next = j;
+
+    q->job_last = j;
+}
+
+bool queue_start_next( queue_t *q )
+{
+    if ( !q->job_next )
+        return false;
+
+    job_t *j = q->job_next;
+    q->job_next = j->next;
+
+    cb_clear( &j->node->deps_dyn );
+    job_fork( j, q->outdir_fd );
+
+    q->running_count ++;
+    q->queued_count --;
+
+    assert( j->pipe_fd < MAX_FD );
+    q->running[ j->pipe_fd ] = j;
+
+    return true;
+}
+
+void queue_skip( queue_t *q, node_t *n )
+{
+    if ( n->failed )
+        return;
+
+    queue_show_result( q, n, NULL );
+    n->failed = true;
+
+    for ( cb_iterator i = cb_begin( &n->blocking ); !cb_end( &i ); cb_next( &i ) )
+        queue_skip( q, cb_get( &i ) );
+}
+
+void queue_cleanup_node( queue_t *q, node_t *n )
+{
+    if ( n->type != out_node && n->type != meta_node || n->dirty || n->waiting )
+        return;
+
+    for ( cb_iterator i = cb_begin( &n->blocking ); !cb_end( &i ); cb_next( &i ) )
+    {
+        node_t *b = cb_get( &i );
+
+        if ( ! -- b->waiting )
+        {
+            if ( b->dirty )
+                queue_add( q, job_add( &q->jobs, b ) );
+            else
+            {
+                queue_cleanup_node( q, b );
+                q->todo_count --;
+            }
+        }
+    }
+
+    cb_clear( &n->blocking );
+}
+
+void queue_cleanup_job( queue_t *q, int fd )
+{
+    job_t *j = q->running[ fd ];
+    q->running[ fd ] = 0;
+    node_t *n = j->node;
+
+    int status;
+
+    if ( waitpid( j->pid, &status, 0 ) == -1 )
+        sys_error( "waitpid %d", j->pid );
+
+    if ( WIFEXITED( status ) && WEXITSTATUS( status ) == 0 )
+    {
+        n->stamp_updated = n->stamp_want;
+        n->cmd_hash = var_hash( n->cmd );
+        n->dirty = false;
+
+        if ( j->changed )
+            n->stamp_changed = n->stamp_want;
+    }
+    else
+        n->failed = true;
+
+    q->running_count --;
+    queue_show_result( q, n, j );
+
+    for ( cb_iterator i = cb_begin( &j->node->blocking ); !cb_end( &i ); cb_next( &i ) )
+    {
+        node_t *b = cb_get( &i );
+
+        if ( n->stamp_changed > b->stamp_updated )
+            b->dirty = true;
+
+        if ( n->failed && !b->failed )
+            queue_skip( q, b );
+
+        if ( -- b->waiting )
+            continue;
+
+        if ( b->dirty && b->stamp_updated < b->stamp_want && !b->failed )
+            queue_add( q, job_add( &q->jobs, b ) );
+        else
+            queue_cleanup_node( q, b );
+    }
+}
+
+void queue_teardown( queue_t *q )
+{
+    fprintf( stderr, "\033[J\r[caught signal %d, cleaning up]\n", _signalled );
+    fflush( stderr );
+
+    job_t *j = NULL;
+
+    for ( int fd = 0; fd < MAX_FD; ++ fd )
+        if ( ( j = q->running[ fd ] ) )
+            kill( j->pid, SIGTERM );
+
+    for ( int fd = 0; fd < MAX_FD; ++ fd )
+        if ( q->running[ fd ] )
+            queue_cleanup_job( q, fd );
+}
+
+bool queue_loop( queue_t *q )
+{
+    fd_set ready, except;
+    FD_ZERO( &ready );
+    FD_ZERO( &except );
+
+    if ( q->running_count )
+    {
+        for ( int i = 0; i < MAX_FD; ++i )
+            if ( q->running[ i ] )
+                FD_SET( i, &ready ), FD_SET( i, &except );
+
+        struct timeval tv = { 1, 0 };
+
+        switch ( select( MAX_FD, &ready, 0, &except, &tv ) )
+        {
+            case 0:
+                return true;
+            case -1:
+                if ( errno != EINTR )
+                    sys_error( "select" );
+            default:
+                ;
+        }
+    }
+
+    if ( _signalled )
+        queue_teardown( q );
+
+    if ( q->running_count )
+        for ( int fd = 0; fd < MAX_FD; ++ fd )
+            if ( FD_ISSET( fd, &ready ) || FD_ISSET( fd, &except ) )
+                if ( job_update( q->running[ fd ], q->nodes, q->srcdir ) )
+                    queue_cleanup_job( q, fd );
+
+    if ( !_signalled )
+        while ( q->running_count < q->running_max )
+            if ( !queue_start_next( q ) )
+                break;
+
+    return !_signalled && ( q->running_count || q->job_next );
+}
+
+void queue_create_jobs( queue_t *q, node_t *goal )
+{
+    if ( goal->visited )
+        return;
+
+    goal->visited = true;
+    node_t *out = goal->type == out_node ? goal : 0;
+
+    for ( cb_iterator i = cb_begin( &goal->deps ); !cb_end( &i ); cb_next( &i ) )
+    {
+        node_t *dep = cb_get( &i );
+        queue_create_jobs( q, dep );
+
+        if ( out && out->stamp_want < dep->stamp_want )
+            out->stamp_want = dep->stamp_want;
+    }
+
+    if ( !out )
+        return;
+
+    if ( var_hash( out->cmd ) != out->cmd_hash )
+        out->dirty = true;
+
+    if ( out->stamp_want > out->stamp_updated || out->dirty )
+    {
+        for ( cb_iterator i = cb_begin( &goal->deps ); !cb_end( &i ); cb_next( &i ) )
+        {
+            node_t *dep = cb_get( &i );
+            node_t *dep_out = dep->type == out_node ? dep : 0;
+
+            if ( dep_out && !dep_out->failed )
+                if ( dep_out->stamp_want > dep_out->stamp_updated || dep_out->dirty )
+                {
+                    cb_insert( &dep->blocking, goal, VSIZE( goal, name ), -1 );
+                    goal->waiting ++;
+                }
+
+            if ( dep->stamp_changed > out->stamp_updated )
+                out->dirty = true;
+        }
+    }
+
+    if ( out->waiting || out->dirty )
+        ++ q->todo_count;
+
+    if ( !out->waiting && out->dirty ) /* can run right away */
+        queue_add( q, job_add( &q->jobs, out ) );
+}
+
+void queue_add_goal( queue_t *q, const char *name )
+{
+    node_t *goal = graph_get( q->nodes, span_lit( name ) );
+
+    if ( goal )
+        queue_create_jobs( q, goal );
+    else
+        error( "goal %s not defined", name );
+}
+
+void queue_init( queue_t *q, int outdir_fd, const char *srcdir )
+{
+    q->outdir_fd = outdir_fd;
+    q->srcdir = srcdir;
+    q->started = time( NULL );
+
+    cb_init( &q->jobs );
+
+    q->failed_count = 0;
+    q->skipped_count = 0;
+    q->ok_count = 0;
+    q->running_count = 0;
+    q->queued_count = 0;
+    q->todo_count = 0;
+    q->running_max = 4;
+
+    q->job_next = NULL;
+    q->job_last = NULL;
+
+    for ( int i = 0; i < MAX_FD; ++i )
+        q->running[ i ] = 0;
+}
+
+void queue_monitor( queue_t *q, bool endmsg )
+{
+    time_t elapsed = 0;
+
+    signal( SIGHUP, sighandler );
+    signal( SIGINT, sighandler );
+    signal( SIGTERM, sighandler );
+
+    signal( SIGPIPE, sighandler ); // ??
+    signal( SIGALRM, sighandler ); // ??
+
+    while ( queue_loop( q ) )
+    {
+        elapsed = time( NULL ) - q->started;
+        fprintf( stderr, "%d/%d running + %d/%d queued | %d ok + %d failed | %lld:%02lld elapsed\r",
+                 q->running_count, q->running_max, q->queued_count, q->todo_count - q->running_count,
+                 q->ok_count, q->failed_count, elapsed / 60, elapsed % 60 );
+    }
+
+    elapsed = time( NULL ) - q->started;
+
+    if ( endmsg )
+        fprintf( stderr, "build finished: %d ok, %d failed, %d skipped, %lld:%02lld elapsed\n",
+                 q->ok_count, q->failed_count, q->skipped_count, elapsed / 60, elapsed % 60 );
+}

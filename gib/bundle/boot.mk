25e99c3f4d93920ebc29ebc2d3ae7db9d1d85b84
Petr Rockai <me@mornfall.net>
2022-02-23 08:10:51 +0000

    gib: Update paths in the boot files.

--- gib/bundle/boot.mk
+++ gib/bundle/boot.mk
@@ -1,15 +1,16 @@
-HDR = gib.bundle/span.h \
-      gib.bundle/critbit.h \
-      gib.bundle/common.h \
-      gib.bundle/env.h \
-      gib.bundle/graph.h \
-      gib.bundle/job.h \
-      gib.bundle/reader.h \
-      gib.bundle/writer.h \
-      gib.bundle/manifest.h \
-      gib.bundle/outdb.h \
-      gib.bundle/rules.h
-SRC = gib.bundle/main.c gib.bundle/sha1.c
+HDR = gib/bundle/span.h \
+      gib/bundle/critbit.h \
+      gib/bundle/common.h \
+      gib/bundle/env.h \
+      gib/bundle/graph.h \
+      gib/bundle/job.h \
+      gib/bundle/queue.h \
+      gib/bundle/reader.h \
+      gib/bundle/writer.h \
+      gib/bundle/manifest.h \
+      gib/bundle/outdb.h \
+      gib/bundle/rules.h
+SRC = gib/bundle/main.c gib/bundle/sha1.c
 
 all $(.TARGETS) $(MAKECMDGOALS): .gib.bin
 	@./.gib.bin $@

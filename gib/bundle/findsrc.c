28bf9b1c9bc6ac587ab0afaf0f1b0f8086f201fe
Petr Rockai <me@mornfall.net>
2022-02-23 12:00:48 +0000

    gib: Make generated manifests (findsrc) depend on encountered directories.

--- gib/bundle/findsrc.c
+++ gib/bundle/findsrc.c
@@ -9,7 +9,7 @@ struct todo
     struct dirent dt;
 };
 
-void dump( int dirfd, writer_t *w, const char *path, bool is_root )
+void dump( int dirfd, writer_t *w, writer_t *dep, const char *path, bool is_root )
 {
     DIR *list = fdopendir( dirfd );
     struct dirent *dirp;
@@ -68,10 +68,14 @@ void dump( int dirfd, writer_t *w, const char *path, bool is_root )
         writer_append( w, span_mk( subpath, spp ) );
         writer_append( w, span_lit( "\n" ) );
 
+        writer_append( dep, span_lit( "dep " ) );
+        writer_append( dep, span_mk( subpath, spp ) );
+        writer_append( dep, span_lit( "\n" ) );
+
         int subfd = openat( dirfd, todo->dt.d_name, O_DIRECTORY | O_RDONLY );
         if ( subfd < 0 )
             sys_error( "opening directory %s", todo->dt.d_name );
-        dump( subfd, w, subpath, false );
+        dump( subfd, w, dep, subpath, false );
 
         struct todo *n = todo->next;
         free( todo );
@@ -86,13 +90,17 @@ int main( int argc, const char *argv[] )
     if ( argc < 2 )
         return 1;
 
-    writer_t out;
+    writer_t out, dep;
     int rootfd = open( argv[ 1 ], O_DIRECTORY | O_RDONLY );
 
     if ( rootfd < 0 )
         sys_error( "opening directory %s", argv[ 1 ] );
 
     writer_open( &out, AT_FDCWD, argv[ 2 ] );
-    dump( rootfd, &out, "", true );
+    dep.file = dep.tmp = NULL;
+    dep.ptr = 0;
+    dep.fd = 3;
+    dump( rootfd, &out, &dep, "", true );
     writer_close( &out );
+    writer_flush( &dep );
 }

2473402bb95dead76176d79a015339f9321cd8a8
Petr Rockai <me@mornfall.net>
2022-02-23 13:19:59 +0000

    gib: Refactor findsrc (move state into a struct).

--- gib/bundle/findsrc.c
+++ gib/bundle/findsrc.c
@@ -9,30 +9,39 @@ struct todo
     struct dirent dt;
 };
 
-void dump( int dirfd, writer_t *w, writer_t *dep, const char *path, bool is_root )
+typedef struct state
 {
-    DIR *list = fdopendir( dirfd );
+    bool is_root;
+    const char *path;
+    int dirfd;
+    writer_t *out, *dep;
+} state_t;
+
+void dump( state_t s )
+{
+    DIR *list = fdopendir( s.dirfd );
     struct dirent *dirp;
     struct todo *todo = 0;
 
     if ( !list )
-        sys_error( "fdopendir on %s failed", path );
+        sys_error( "fdopendir on %s failed", s.path );
 
     while ( ( dirp = readdir( list ) ) )
     {
-        if ( dirp->d_name[ 0 ] == '.' )
-            continue;
+        int last = strlen( dirp->d_name ) - 1;
 
-        if ( is_root && ( !strncmp( dirp->d_name, "bin.", 4 ) ||
-                          !strncmp( dirp->d_name, "gib.", 4 ) ||
-                          dirp->d_name[ 0 ] == '_' ) )
+        if ( dirp->d_name[ 0 ] == '.' ||
+             dirp->d_name[ last ] == '~' ||
+             s.is_root && dirp->d_name[ 0 ] == '_' ||
+             s.is_root && !strcmp( dirp->d_name, "gibfile" ) ||
+             s.is_root && !strcmp( dirp->d_name, "gib" ) )
             continue;
 
         if ( dirp->d_type == DT_UNKNOWN )
         {
             struct stat st;
 
-            if ( fstatat( dirfd, dirp->d_name, &st, 0 ) )
+            if ( fstatat( s.dirfd, dirp->d_name, &st, 0 ) )
                 sys_error( "fstatat %s", dirp->d_name );
 
             if ( S_ISREG( st.st_mode ) ) dirp->d_type = DT_REG;
@@ -49,34 +58,40 @@ void dump( int dirfd, writer_t *w, writer_t *dep, const char *path, bool is_root
 
         if ( dirp->d_type == DT_REG )
         {
-            writer_append( w, span_lit( "f " ) );
-            writer_append( w, span_lit( dirp->d_name ) );
-            writer_append( w, span_lit( "\n" ) );
+            writer_append( s.out, span_lit( "f " ) );
+            writer_append( s.out, span_lit( dirp->d_name ) );
+            writer_append( s.out, span_lit( "\n" ) );
         }
     }
 
-    writer_append( w, span_lit( "\n" ) );
+    writer_append( s.out, span_lit( "\n" ) );
+
+    state_t sub = s;
+    sub.is_root = false;
 
     while ( todo )
     {
-        char subpath[ strlen( path ) + strlen( todo->dt.d_name ) + 2 ], *spp = subpath;
-        spp = stpcpy( spp, path );
-        if ( strlen( path ) )
+        char subpath[ strlen( s.path ) + strlen( todo->dt.d_name ) + 2 ], *spp = subpath;
+        spp = stpcpy( spp, s.path );
+        if ( strlen( s.path ) )
             *spp++ = '/';
         spp = stpcpy( spp, todo->dt.d_name );
 
-        writer_append( w, span_lit( "d " ) );
-        writer_append( w, span_mk( subpath, spp ) );
-        writer_append( w, span_lit( "\n" ) );
+        writer_append( s.out, span_lit( "d " ) );
+        writer_append( s.out, span_mk( subpath, spp ) );
+        writer_append( s.out, span_lit( "\n" ) );
 
-        writer_append( dep, span_lit( "dep " ) );
-        writer_append( dep, span_mk( subpath, spp ) );
-        writer_append( dep, span_lit( "\n" ) );
+        writer_append( s.dep, span_lit( "dep " ) );
+        writer_append( s.dep, span_mk( subpath, spp ) );
+        writer_append( s.dep, span_lit( "\n" ) );
 
-        int subfd = openat( dirfd, todo->dt.d_name, O_DIRECTORY | O_RDONLY );
-        if ( subfd < 0 )
+        sub.path   = subpath;
+        sub.dirfd  = openat( s.dirfd, todo->dt.d_name, O_DIRECTORY | O_RDONLY );
+
+        if ( sub.dirfd < 0 )
             sys_error( "opening directory %s", todo->dt.d_name );
-        dump( subfd, w, dep, subpath, false );
+
+        dump( sub );
 
         struct todo *n = todo->next;
         free( todo );
@@ -92,16 +107,26 @@ int main( int argc, const char *argv[] )
         error( "usage: %s <rootdir> <outfile>", argv[ 0 ] );
 
     writer_t out, dep;
-    int rootfd = open( argv[ 1 ], O_DIRECTORY | O_RDONLY );
 
-    if ( rootfd < 0 )
+    state_t s =
+    {
+        .dirfd   = open( argv[ 1 ], O_DIRECTORY | O_RDONLY ),
+        .out     = &out,
+        .dep     = &dep,
+        .is_root = true,
+        .path    = ""
+    };
+
+    if ( s.dirfd < 0 )
         sys_error( "opening directory %s", argv[ 1 ] );
 
     writer_open( &out, AT_FDCWD, argv[ 2 ] );
     dep.file = dep.tmp = NULL;
     dep.ptr = 0;
     dep.fd = 3;
-    dump( rootfd, &out, &dep, "", true );
+
+    dump( s );
+
     writer_close( &out );
     writer_flush( &dep );
 }

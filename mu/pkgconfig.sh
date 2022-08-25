c4b114d23ce90fcd3949058c1e7e20211781ea0f
Petr Rockai <code@fixp.eu>
2022-08-25 21:55:40 +0000

    mu: Add an embeddable build recipe (for gib).

---
+++ mu/pkgconfig.sh
@@ -0,0 +1,5 @@
+{
+    echo "add cxxflags.mu $(pkg-config --cflags poppler-glib icu-uc)"
+    echo "add ldflags.svgtex $(pkg-config --libs poppler-glib)"
+    echo "add ldflags.mu $(pkg-config --libs icu-uc)"
+} > $1

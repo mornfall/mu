daf44571e90ea4e7518135b783ece4f7f82e7d7f
Petr Rockai <code@fixp.eu>
2022-11-07 20:30:13 +0000

    mu: Make pkgconfig.sh actually fail for real when pkg-config fails.

--- mu/pkgconfig.sh
+++ mu/pkgconfig.sh
@@ -1,7 +1,7 @@
 set -e
 
 {
-    echo "add cxxflags.mu $(pkg-config --cflags poppler-glib icu-uc)"
-    echo "add ldflags.svgtex $(pkg-config --libs poppler-glib)"
-    echo "add ldflags.mu $(pkg-config --libs icu-uc)"
+    printf "add cxxflags.mu "; pkg-config --cflags poppler-glib icu-uc
+    printf "add ldflags.svgtex "; pkg-config --libs poppler-glib
+    printf "add ldflags.mu "; pkg-config --libs icu-uc
 } > $1

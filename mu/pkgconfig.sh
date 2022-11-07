88444d860cb2e7364374a5bd708bdf3bf62163d7
Jan Juracka <xjurack1@fi.muni.cz>
2022-11-07 18:51:25 +0100

    fix: building will not long continue when pkgconfig script fails

--- mu/pkgconfig.sh
+++ mu/pkgconfig.sh
@@ -1,7 +1,15 @@
 set -e
 
+# args: value, expression
+add_config()
 {
-    echo "add cxxflags.mu $(pkg-config --cflags poppler-glib icu-uc)"
-    echo "add ldflags.svgtex $(pkg-config --libs poppler-glib)"
-    echo "add ldflags.mu $(pkg-config --libs icu-uc)"
+    if ! [ $# -eq 2 ]; then >&2 echo pkg-config script error; exit 1; fi
+    _tmp="$($2)"
+    echo "add $1 $_tmp"
+}
+
+{
+    add_config cxxflags.mu "pkg-config --cflags poppler-glib icu-uc"
+    add_config ldflags.svgtex "pkg-config --libs poppler-glib"
+    add_config ldflags.mu "pkg-config --libs icu-uc"
 } > $1

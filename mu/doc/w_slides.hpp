38232ccf3ec9111a1df079ecfefbca2738ed12fb
Petr Rockai <code@fixp.eu>
2023-02-16 11:01:11 +0000

    mu doc: Rework w_slides to do slide extraction itself.

--- mu/doc/w_slides.hpp
+++ mu/doc/w_slides.hpp
@@ -9,6 +9,55 @@ namespace umd::doc
 {
     struct w_slides : w_context
     {
-        w_slides( stream &out ) : w_context( out ) { format = U"slides"; }
+        int last_head = 0;
+        bool in_slide = false;
+
+        w_slides( stream &out ) : w_context( out )
+        {
+            format = U"slides";
+        }
+
+        void meta_end() override
+        {
+            w_context::meta_end();
+            out.buffer( "not_slides" );
+        }
+
+        void heading_start( int l, sv num ) override
+        {
+            last_head = l;
+            slide_start();
+            w_context::heading_start( l, num );
+        }
+
+        void heading_stop()
+        {
+            w_context::heading_stop();
+            if ( last_head < 3 )
+                slide_end();
+        }
+
+        void mpost_start() override { slide_start(); w_context::mpost_start(); }
+        void mpost_stop() override  { w_context::mpost_stop(); slide_end(); }
+        void nest_start() override  { slide_start(); w_context::nest_start(); }
+        void nest_end() override    { w_context::nest_end(); slide_end(); }
+
+        void slide_start()
+        {
+            if ( !in_slide )
+            {
+                out.resume();
+                out.emit( "\\startslide\n" );
+                in_slide = true;
+            }
+        }
+
+        void slide_end()
+        {
+            ASSERT( in_slide );
+            out.emit( "\\stopslide\n" );
+            out.buffer( "not_slides" );
+            in_slide = false;
+        }
     };
 }

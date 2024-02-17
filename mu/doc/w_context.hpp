4cdbe8416d58379de844c590d8cab00054ed6898
Petr Rockai <code@fixp.eu>
2024-02-17 19:36:13 +0000

    mu tex: Tighten vertical spacing in tables a little.

--- mu/doc/w_context.hpp
+++ mu/doc/w_context.hpp
@@ -153,7 +153,7 @@ namespace umd::doc
             for ( auto c : ci )
                 out.emit( "\\setupTABLE[c][", std::to_string( ++i ), "][", setup( c ), "]\n" );
 
-            out.emit( "\\beforetable\\bTABLE[boffset=0pt,loffset=\\tablemargin,roffset=\\tablemargin]",
+            out.emit( "\\beforetable\\bTABLE[loffset=\\tablemargin,roffset=\\tablemargin]",
                       table_rules ? "[bottomframe=on]" : "" );
         }
 
@@ -162,7 +162,7 @@ namespace umd::doc
             if ( ++table_cells > 1 )
                 out.emit( "\\eTD " );
             out.emit( "\\bTD[nc=", std::to_string( span ), span > 1 ? ",align={center}" : "",
-                      table_rules ? ",topframe=on" : ",toffset=0pt", "]" );
+                      table_rules ? ",topframe=on" : ",toffset=-1pt", "]" );
         }
 
         void table_new_row( bool rule = false ) override
